300b6ce48b18ca6ecfc39e07c1bab05b53ac2534
choiyongseok90 <choi.yongseok@yanolja.com>
2022-10-02 12:25:46 +0900

    feat: 게임 기능 추가
    게임준비, 게임, 게임 상태확인, 리셋 등의 기본 게임 기능을 구현.
    출력은 무조건 Output을 통해서만 하도록 구성
    우선적으로 Reset은 결과만 reset하는 것과 game 전체의 restart 하는것을 나눠뒀음

---
+++ src/main/java/baseball/domain/game/play/Game.java
@@ -0,0 +1,54 @@
+package baseball.domain.game.play;
+
+
+import baseball.domain.game.result.Result;
+import baseball.domain.messages.PlayMessage;
+import baseball.view.Output;
+
+import java.util.List;
+
+/**
+ * @info 게임 전체의 서비스를 관장하는 Game 클래스
+ * @action 메인 메서드인 play에서는 사용자와 컴퓨터의 값을 받아 결과를 생성하고, 메시지를 보여주고, 종료여부를 업데이트함
+ * @comment 게임의 기본은 게임준비, 게임, 결과 리셋, 리플레이 - 플레이어 입력받는 부분은 controller로 뺐음
+ * @author YONGSEOK CHOI
+ * @version 1.0 2022.10.02
+ */
+public class Game {
+    private Result result;
+    private GameStatus status;
+
+
+    public Game(){
+        this.result = new Result();
+        this.status = status.PLAYING;
+    }
+
+    public void play(List<Integer> computerNumbers, List<Integer> playerNumbers){
+        result.checkResult(computerNumbers, playerNumbers);
+        showMessage();
+        checkFinish();
+    }
+    private void checkFinish() {
+        if(result.isFinished()){
+            Output.printGamePlayMessage(PlayMessage.GAME_FINISH);
+            this.status = GameStatus.FINISHED;
+        }
+    }
+
+    public boolean isPlaying() {
+        return this.status == GameStatus.PLAYING;
+    }
+
+    public void restartGame() {
+        this.result.reset();
+        this.status = status.PLAYING;
+    }
+
+    public void resetResult(){
+        this.result.reset();
+    }
+    private void showMessage() {
+        Output.printGameResultMessage(result.getResultMessage());
+    }
+}
